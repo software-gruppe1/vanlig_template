494dd59f14d6450585ecb102d04da489085b375c
Villa25698 <111729489+Villa25698@users.noreply.github.com>
2023-09-12 11:07:48 +0200

    Add files via upload
    vanlig template for backend

---
+++ software_engineer/src/test/java/com/example/software_engineer/SoftwareEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.software_engineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
