8dd5ddcdea2c3db75d790002aa01df47ceb5ac7e
Villa25698 <111729489+Villa25698@users.noreply.github.com>
2023-09-12 11:35:38 +0200

    test

---
+++ src/main/java/com/example/software_engineer/SoftwareEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.example.software_engineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareEngineerApplication.class, args);
+    }
+
+}
