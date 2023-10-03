601323aaf2768f4aacd79104a2cfc581aacc3466
mahamoudibrahim <mahamod.ibrahim@gmail.com>
2023-10-03 12:15:20 +0200

    Begynte å legge til instansvariabler i User klassen

--- src/main/java/com/example/software_engineer/model/User.java
+++ src/main/java/com/example/software_engineer/model/User.java
@@ -1,4 +1,8 @@
 package com.example.software_engineer.model;
 
 public class User implements Account{
+    private String user_name, first_name, last_name;
+    private ShoppingCart shopping_cart;
+    private Order order;
+
 }
