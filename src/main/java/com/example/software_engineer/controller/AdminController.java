84303d898066ac133646a39e58bd7f343ae01150
Fredrbr <fredbro@hotmail.no>
2023-11-04 20:38:37 +0100

    -Mapping for metodene i json repository.

--- src/main/java/com/example/software_engineer/controller/AdminController.java
+++ src/main/java/com/example/software_engineer/controller/AdminController.java
@@ -2,19 +2,40 @@ package com.example.software_engineer.controller;
 
 
 import com.example.software_engineer.model.Account;
+import com.example.software_engineer.model.Services;
+import com.example.software_engineer.repository.JSONRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
-    @DeleteMapping("/removeUser/{userId}")
-    public String removeUser(@PathVariable Long userId) {
-        return userId + " Removed";
+    private final JSONRepository jsonRepository;
+
+    public AdminController(JSONRepository jsonRepository) {
+        this.jsonRepository = jsonRepository;
     }
 
-    @DeleteMapping("deletead/{adId}")
-    public String deleteAd(@PathVariable Long adId) {
-        return adId + " Removed";
+    @GetMapping("/services")
+    public ResponseEntity<List<Services>> getAllServices() {
+        List<Services> services = jsonRepository.allServices();
+        return new ResponseEntity<>(services, HttpStatus.OK);
+    }
+
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        List<Account> accounts = jsonRepository.allAccount();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/services/{UID}")
+    public ResponseEntity<Void> deleteService(@PathVariable String UID) {
+        jsonRepository.deleteService(null, UID);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
