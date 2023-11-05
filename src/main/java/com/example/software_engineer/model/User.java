a4abef516055aece1399dba45561b097309aed5b
Ayubaa <Ayubaa@hiof.no>
2023-11-05 19:30:23 +0100

    Måtte endre litt på user og Admin fordi de arver fra account

--- src/main/java/com/example/software_engineer/model/User.java
+++ src/main/java/com/example/software_engineer/model/User.java
@@ -4,12 +4,10 @@ public class User extends Account{
 
 
     public User (String fullName, String password, String email,
-                 ShoppingCart shopping_cart){
-        super( fullName, password, email, shopping_cart);
+                 ShoppingCart shopping_cart, String username){
+        super( fullName, password, email, shopping_cart, username);
 
     }
 
-    public String getUsername() {
-        return "User";
-    }
+
 }
