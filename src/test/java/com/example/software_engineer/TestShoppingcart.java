8feb7c26e9cca65b2bb59eed4ce982e241415fe4
Ayubaa <Ayubaa@hiof.no>
2023-11-16 10:19:47 +0100

    Lagde 3 forbedret tester i shoppingcarttest

--- src/test/java/com/example/software_engineer/TestShoppingcart.java
+++ src/test/java/com/example/software_engineer/TestShoppingcart.java
@@ -4,6 +4,7 @@ import com.example.software_engineer.model.Reviews;
 import com.example.software_engineer.model.Services;
 import com.example.software_engineer.model.ShoppingCart;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -13,86 +14,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestShoppingcart {
-
-    @Nested
-    class add_to_shoppingcart{
-        @Test
-        public void isServiceinShoppingCart() {
-            //Gjør totalprice til 0 fordi shoppingcarten skal starte med 0
-            int totalprice= 0;
-            //Lager en arraylist for serviceUID
-            ArrayList<String> servicesUID = new ArrayList<>();
-
-            //Lager et objekt for å bruke og teste den
-            Services serviceInfo = new Services("Cowabonga", LocalDate.of(2003, 2, 15), 165,
-                    "Awesome place","Norway", "ayubs", "123DSASDsf");
-
-            //legger til objekten inn i arraylisten
-            servicesUID.add(serviceInfo.getUID());
-
-            //Har en if test som verifiserer om den har blitt lagt til så skal totalprisen oppdatere seg.
-            if (servicesUID.add(serviceInfo.getUID())){
-                totalprice+= serviceInfo.getPrice();
-            }
-
-            //TESTENE
-            Assertions.assertEquals(servicesUID.get(0), serviceInfo.getUID() );
-            Assertions.assertEquals(165, totalprice);
-
-
-
-        }
-
-
+    ShoppingCart shoppingCart;
+    @BeforeEach
+    public void setUp() {
+        // Create an instance of ShoppingCart before each test
+        shoppingCart = new ShoppingCart();
     }
     @Nested
-    class delete_from_shoppingcartTEST {
+    class add_to_shoppingcart1{
         @Test
-        public void hasServiceleftShoppingCart() {
-            //tenker å bruke en del av min forje test på denne testen.
-            int totalprice= 0;
 
-            ArrayList<String> servicesUID = new ArrayList<>();
-
-
-            //Lager et objekt for å bruke og teste den
+        public void isServiceinShoppingCart() {
             Services serviceInfo1 = new Services("Cowabonga", LocalDate.of(2003, 2, 15), 165,
                     "Awesome place", "Norway", "ayubs", "123DSASDsf");
 
-            //legger til objekten inn i arraylisten
-            servicesUID.add(serviceInfo1.getUID());
-            //Har en if test som verifiserer om den har blitt lagt til så skal totalprisen oppdatere seg.
-            if (servicesUID.add(serviceInfo1.getUID())){
-                totalprice+= serviceInfo1.getPrice();
-            }
-            Assertions.assertEquals(servicesUID.get(0), serviceInfo1.getUID() );
-            Assertions.assertEquals(165, totalprice);
+            shoppingCart.add_services(serviceInfo1, serviceInfo1.getUID());
 
-            //Første ordentlig test uten å lage en metode!
-            //Dette er en test for en metode som skal slette en service fra en arraylist
-            servicesUID.remove(serviceInfo1.getUID());
-
-            if(servicesUID.remove(serviceInfo1.getUID())){
-                totalprice-=serviceInfo1.getPrice();
-            }
-
-            Assertions.assertEquals(0, servicesUID.size());
-            Assertions.assertEquals(0, totalprice);
-
-            //Virker som at jeg faktisk ikke har brukt metoden i det hele tatt som jeg tenker er litt feil
-            //Gjorde ikke det på første test heller.
+            Assertions.assertEquals(165, shoppingCart.getTotalPrice() );
+            Assertions.assertEquals(1, shoppingCart.getServices().size());
 
         }
+    }
+
+    @Nested
+    class delete_service{
+        @Test
+
+        public void delete_from_shoppingcartTEST() {
+            Services serviceInfo1 = new Services("Cowabonga", LocalDate.of(2003, 2, 15), 165,
+                    "Awesome place", "Norway", "ayubs", "123DSASDsf");
+
+            shoppingCart.add_services(serviceInfo1, serviceInfo1.getUID());
+
+            //Har testet denne metoden tidligere, så jeg vet hva som skjer
+
+            shoppingCart.delete_service(serviceInfo1, serviceInfo1.getUID());
+
+            Assertions.assertEquals(0, shoppingCart.getTotalPrice());
+            Assertions.assertEquals(0, shoppingCart.getServices().size());
+        }
+
+
+
 
     }
     @Nested
     class clearShoppingcartTEST {
         @Test
         public void hasALLServicesleftShoppingCart() {
-            //tenker å bruke en del av min forje test på denne testen.
-            int totalprice= 0;
 
-            ArrayList<String> servicesUID = new ArrayList<>();
+
+
+
 
 
             //Lager et objekt for å bruke og teste den
@@ -102,29 +75,19 @@ public class TestShoppingcart {
                     "horrible place", "France", "sbuya", "321DSASDfs");
 
             //legger til objekten inn i arraylisten
-            servicesUID.add(serviceInfo1.getUID());
-            servicesUID.add(serviceInfo2.getUID());
+            shoppingCart.add_services(serviceInfo1, serviceInfo1.getUID());
+            shoppingCart.add_services(serviceInfo2, serviceInfo2.getUID());
             //Har en if test som verifiserer om den har blitt lagt til så skal totalprisen oppdatere seg.
 
-            totalprice+= serviceInfo1.getPrice(); //Må lage en for loop her
-
-
-            totalprice+= serviceInfo2.getPrice(); //Må putte disse i en for loop med metoden.
-
-
-            Assertions.assertEquals(2, servicesUID.size());
-            Assertions.assertEquals(5165, totalprice);
+            Assertions.assertEquals(2, shoppingCart.getServices().size());
+            Assertions.assertEquals(5165, shoppingCart.getTotalPrice());
 
             //Andre ordentlig test uten å lage en metode!
             //Dette er en test for en metode som skal slette en service fra en arraylist
-            servicesUID.clear();
+            shoppingCart.delete_shoppingcart();
 
-            if(servicesUID.isEmpty()){
-                totalprice= 0;
-            }
-
-            Assertions.assertEquals(0, servicesUID.size());
-            Assertions.assertEquals(0, totalprice);
+            Assertions.assertEquals(0, shoppingCart.getServices().size());
+            Assertions.assertEquals(0, shoppingCart.getTotalPrice());
 
             //Virker som at jeg faktisk ikke har brukt metoden i det hele tatt som jeg tenker er litt feil
             //Gjorde ikke det på første test heller.
@@ -132,4 +95,7 @@ public class TestShoppingcart {
         }
 
     }
+
+
+
 }
